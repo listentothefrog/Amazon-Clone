070d6139a831ddc45c1468b4ce47181e4fef3956
DevGenix <you@example.com>
2020-10-11 21:54:55 -0400

    Added firebase auth features

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Checkout from "./components/Checkout";
 import Login from "./components/Login";
+import { auth } from "./firebase";
+import { useStateValue } from "./components/StateProvider";
 
 function App() {
+  const [{}, dispatch] = useStateValue();
+  useEffect(() => {
+    // will only run if the app component loads....
+    auth.onAuthStateChanged((authUser) => {
+      console.log("The user is >>>", authUser);
+      if (authUser) {
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
   return (
     // BEM
     <Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,37 @@
 import React, { useState } from "react";
 import "./styles/Login.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { useStateValue } from "./StateProvider";
+import { Link, useHistory } from "react-router-dom";
+import { auth } from "../firebase";
 
 function Login() {
+  const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+
   const signIn = (e) => {
     e.preventDefault();
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
+
+  const register = (e) => {
+    e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        // it successfully created a new user with email and password
+        if (auth) {
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <div className="login">
       <Link to="/">
@@ -20,7 +42,8 @@ function Login() {
       </Link>
 
       <div className="login__container">
-        <h1>Sign In</h1>
+        <h1>Sign-in</h1>
+
         <form>
           <h5>E-mail</h5>
           <input
@@ -28,12 +51,14 @@ function Login() {
             value={email}
             onChange={(e) => setEmail(e.target.value)}
           />
+
           <h5>Password</h5>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
           />
+
           <button
             type="submit"
             onClick={signIn}
@@ -42,13 +67,15 @@ function Login() {
             Sign In
           </button>
         </form>
+
         <p>
-          By signing-in you agree to the AMAZON CLONE Conditions of Use & Sale.
-          Please see our Privacy Notice, our Cookies Notice and our
+          By signing-in you agree to the AMAZON FAKE CLONE Conditions of Use &
+          Sale. Please see our Privacy Notice, our Cookies Notice and our
           Interest-Based Ads Notice.
         </p>
-        <button className="login__registerButton">
-          Create an Amazon Account
+
+        <button onClick={register} className="login__registerButton">
+          Create your Amazon Account
         </button>
       </div>
     </div>
